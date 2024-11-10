75e6e903112604fbc9a0a8327fa35d3c5fea0c4d
avinash-anand <avinash.mait@gmail.com>
2024-11-10 11:57:43 +0530

    3240 solved

---
+++ src/test/java/com/example/leetcode/learn/Problem3240Test.java
@@ -0,0 +1,25 @@
+package com.example.leetcode.learn;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+class Problem3240Test {
+
+    @Test
+    void testcase1() {
+        int[] nums = {-4, -1, 0, 3, 10};
+        int[] expected = {0, 1, 9, 16, 100};
+        int[] result = new Problem3240().sortedSquares(nums);
+        assertArrayEquals(expected, result);
+    }
+
+    @Test
+    void testcase2() {
+        int[] nums = {-7, -3, 2, 3, 11};
+        int[] expected = {4, 9, 9, 49, 121};
+        int[] result = new Problem3240().sortedSquares(nums);
+        assertArrayEquals(expected, result);
+    }
+
+}
